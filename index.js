1e67395d5e42c035f05b4987454c9b74c0309bb1
Gabriel Figueiredo <eugfl@hotmail.com>
2024-06-13 02:17:05 -0300

    feat: add helper functions to user controllers

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import 'dotenv/config.js'
 import express from 'express'
-import { CreateUserController } from './src/controllers/create-use.js'
+import { CreateUserController } from './src/controllers/create-user.js'
 import { GetUserByIdController } from './src/controllers/get-user-by-id.js'
 import { UpdateUserController } from './src/controllers/update-user.js'
 
---
+++ src/controllers/helpers/user.js
@@ -0,0 +1,24 @@
+import validator from 'validator'
+import { badRequest } from './http'
+
+export const invalidPasswordResponse = () => {
+    badRequest({
+        message: 'Password must be at least 6 characters',
+    })
+}
+
+export const emailIsAlreadyInUseResponse = () => {
+    badRequest({
+        message: 'Invalid e-mail. Please provide a valid one',
+    })
+}
+
+export const invalidIdResponse = () => {
+    badRequest({
+        message: 'The provided id is not valid',
+    })
+}
+
+export const checkIfPasswordIsValid = (password) => password.length >= 6
+
+export const checkIfEmailIsValid = (email) => validator.isEmail(email)
