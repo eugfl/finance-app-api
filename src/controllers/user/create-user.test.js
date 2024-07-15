af070fc7e8911f0c516d35f1b971af0c46c1741c
Gabriel Figueiredo <eugfl@hotmail.com>
2024-07-14 22:37:52 -0300

    test: ensure 201 is returned when user is created

---
+++ src/controllers/user/create-user.test.js
@@ -0,0 +1,32 @@
+import { CreateUserController } from './create-user'
+
+describe('Create User Controller', () => {
+    class CreateUserUseCaseStub {
+        execute(user) {
+            return user
+        }
+    }
+
+    it('should return 201 when creating a user successfully', async () => {
+        // arange
+        const createUserUseCase = new CreateUserUseCaseStub()
+        const createUserController = new CreateUserController(createUserUseCase)
+
+        const httpResquest = {
+            body: {
+                first_name: 'Gabriel',
+                last_name: 'Figueiredo',
+                email: 'eugfl@gmail.com',
+                password: '123456',
+            },
+        }
+
+        // act
+
+        const result = await createUserController.execute(httpResquest)
+
+        // assert
+        expect(result.statusCode).toBe(201)
+        expect(result.body).toBe(httpResquest.body)
+    })
+})
