d5a39b23dd6043b794ba629a1cd83d395498e216
Gabriel Figueiredo <eugfl@hotmail.com>
2024-07-29 11:45:57 -0300

    test: ensure CreateUserController returns 500 if CreateUserUseCase throws EmailAlreadyInUseError

--- src/controllers/user/create-user.test.js
+++ src/controllers/user/create-user.test.js
@@ -1,5 +1,6 @@
 import { faker } from '@faker-js/faker'
 import { CreateUserController } from './create-user'
+import { EmailAlreadyInUseError } from '../../errors/user'
 
 describe('Create User Controller', () => {
     class CreateUserUseCaseStub {
@@ -199,5 +200,28 @@ describe('Create User Controller', () => {
 
         // assert
         expect(result.statusCode).toBe(500)
+    })
+
+    it('should return 500 if CreateUserUseCase throws EmailIsAlreadyInUseError', async () => {
+        // arange
+        const createUserUseCase = new CreateUserUseCaseStub()
+        const createUserController = new CreateUserController(createUserUseCase)
+        const httpResquest = {
+            body: {
+                first_name: faker.person.firstName(),
+                last_name: faker.person.lastName(),
+                email: faker.internet.email(),
+                password: faker.internet.password({ length: 7 }),
+            },
+        }
+        jest.spyOn(createUserUseCase, 'execute').mockImplementationOnce(() => {
+            throw new EmailAlreadyInUseError(httpResquest.body.email)
+        })
+
+        // act
+        const result = await createUserController.execute(httpResquest)
+
+        // assert
+        expect(result.statusCode).toBe(400)
     })
 })
