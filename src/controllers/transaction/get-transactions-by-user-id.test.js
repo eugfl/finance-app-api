848e41250eefd26b1007006fa120e2920a13915f
Gabriel Figueiredo <eugfl@hotmail.com>
2024-07-30 17:25:37 -0300

    test: ensure GetTransactionsByUserIdController returns 200 when finding user transactions

---
+++ src/controllers/transaction/get-transactions-by-user-id.test.js
@@ -0,0 +1,42 @@
+import { GetTransactionsByUserIdController } from './get-transactions-by-user-id'
+import { faker } from '@faker-js/faker'
+
+describe('GetTransactionsByUserIdController', () => {
+    class GetTransactionsByUserIdUseCaseStub {
+        async execute() {
+            return [
+                {
+                    user_id: faker.string.uuid(),
+                    id: faker.string.uuid(),
+                    name: faker.commerce.productName(),
+                    date: faker.date.anytime().toISOString(),
+                    type: 'EXPENSE',
+                    amount: Number(faker.finance.amount()),
+                },
+            ]
+        }
+    }
+
+    const makeSut = () => {
+        const getTransactionsByUserIdUseCase =
+            new GetTransactionsByUserIdUseCaseStub()
+        const sut = new GetTransactionsByUserIdController(
+            getTransactionsByUserIdUseCase,
+        )
+
+        return { getTransactionsByUserIdUseCase, sut }
+    }
+
+    it('should return 200 when finding transaction by user id successfully', async () => {
+        //arrange
+        const { sut } = makeSut()
+
+        //act
+        const result = await sut.execute({
+            query: { userId: faker.string.uuid() },
+        })
+
+        //assert
+        expect(result.statusCode).toBe(200)
+    })
+})
