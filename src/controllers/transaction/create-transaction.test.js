288abfc64e13f1c602656e2ac79ff74d42e6b4f6
Gabriel Figueiredo <eugfl@hotmail.com>
2024-07-30 14:08:00 -0300

    test: ensure CreateTransactionController returns 500 when CreateTransactionUseCase throws

--- src/controllers/transaction/create-transaction.test.js
+++ src/controllers/transaction/create-transaction.test.js
@@ -195,4 +195,18 @@ describe('CreateTransactionController', () => {
         //assert
         expect(result.statusCode).toBe(400)
     })
+
+    it('should return 500 when CreateTransactionUseCase throws', async () => {
+        //arrange
+        const { sut, createTransactionUseCase } = makeSut()
+        jest.spyOn(createTransactionUseCase, 'execute').mockRejectedValueOnce(
+            new Error(),
+        )
+
+        //act
+        const result = await sut.execute(baseHttpRequest)
+
+        //assert
+        expect(result.statusCode).toBe(500)
+    })
 })
