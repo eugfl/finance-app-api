53a52c813d1c737eb0f3685ab487277046f8d87e
Gabriel Figueiredo <eugfl@hotmail.com>
2024-07-30 17:39:12 -0300

    test: ensure GetTransactionsByUserIdController returns 500 when use case throws generic error

--- src/controllers/transaction/get-transactions-by-user-id.test.js
+++ src/controllers/transaction/get-transactions-by-user-id.test.js
@@ -83,4 +83,21 @@ describe('GetTransactionsByUserIdController', () => {
         //assert
         expect(result.statusCode).toBe(404)
     })
+
+    it('should return 500 when GetTransactionByUserIdUseCase throws generic error', async () => {
+        //arrange
+        const { sut, getTransactionsByUserIdUseCase } = makeSut()
+        jest.spyOn(
+            getTransactionsByUserIdUseCase,
+            'execute',
+        ).mockRejectedValueOnce(new Error())
+
+        //act
+        const result = await sut.execute({
+            query: { userId: faker.string.uuid() },
+        })
+
+        //assert
+        expect(result.statusCode).toBe(500)
+    })
 })
