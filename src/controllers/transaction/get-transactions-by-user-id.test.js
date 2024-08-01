09186d7f17ca19e7e6faca32820eb35de302f9dc
Gabriel Figueiredo <eugfl@hotmail.com>
2024-08-01 18:25:31 -0300

    test: ensure GetTransactionsByUserIdController calls use case with correct params

--- src/controllers/transaction/get-transactions-by-user-id.test.js
+++ src/controllers/transaction/get-transactions-by-user-id.test.js
@@ -100,4 +100,19 @@ describe('GetTransactionsByUserIdController', () => {
         //assert
         expect(result.statusCode).toBe(500)
     })
+
+    it('should call GetTransactionByUserIdUseCase with correct params', async () => {
+        //arrange
+        const { sut, getTransactionsByUserIdUseCase } = makeSut()
+        const executeSpy = jest.spyOn(getTransactionsByUserIdUseCase, 'execute')
+
+        const userId = faker.string.uuid()
+        //act
+        await sut.execute({
+            query: { userId: userId },
+        })
+
+        //assert
+        expect(executeSpy).toHaveBeenCalledWith(userId)
+    })
 })
