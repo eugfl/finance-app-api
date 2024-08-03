f882dd5e83065dd78de281730f73babc5ce45830
Gabriel Figueiredo <eugfl@hotmail.com>
2024-08-02 23:01:10 -0300

    feat: ensure CreateTransactionUseCase uses IdGeneratorAdapter

--- src/factories/controllers/transaction.js
+++ src/factories/controllers/transaction.js
@@ -1,3 +1,4 @@
+import { IdGeneratorAdapter } from '../../adapters/index.js'
 import {
     CreateTransactionController,
     GetTransactionsByUserIdController,
@@ -22,12 +23,13 @@ import {
 export const makeCreateTransactionController = () => {
     const createTransactionRepository =
         new PostgresCreateTransactionRepository()
-
     const getUserByIdRepository = new PostgresGetUserByIdRepository()
+    const idGeneratorAdapter = new IdGeneratorAdapter()
 
     const createTransactionUseCase = new CreateTransactionUseCase(
         createTransactionRepository,
         getUserByIdRepository,
+        idGeneratorAdapter,
     )
 
     const createTransactionController = new CreateTransactionController(
