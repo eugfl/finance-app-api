2febbc3ea85ead1418b2a33ad177b9b872c8566e
Gabriel Figueiredo <eugfl@hotmail.com>
2024-08-02 23:06:45 -0300

    refactor: remove unused code

--- src/controllers/helpers/user.js
+++ src/controllers/helpers/user.js
@@ -1,16 +1,4 @@
-import { badRequest, notFound } from './http.js'
-
-export const invalidPasswordResponse = () => {
-    return badRequest({
-        message: 'Password must be at least 6 characters',
-    })
-}
-
-export const emailIsAlreadyInUseResponse = () => {
-    return badRequest({
-        message: 'Invalid e-mail. Please provide a valid one',
-    })
-}
+import { notFound } from './http.js'
 
 export const userNotFoundResponse = () => {
     return notFound({
