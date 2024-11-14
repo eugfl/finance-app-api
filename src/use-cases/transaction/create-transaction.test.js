ec6091b1c573962744313d500f3bf3ce4175ea95
Gabriel Figueiredo <eugfl@hotmail.com>
2024-11-14 16:19:36 -0300

    test: ensure CreateTransactionUseCase calls IdGeneratorAdapter

--- src/use-cases/transaction/create-transaction.test.js
+++ src/use-cases/transaction/create-transaction.test.js
@@ -84,4 +84,16 @@ describe('CreateTransactionUseCase', () => {
             createTransactionParams.user_id,
         )
     })
+
+    it('should call IdGeneratorAdapter', async () => {
+        //arrange
+        const { sut, idGeneratorAdapter } = makeSut()
+        const idGeneratorAdapterSpy = jest.spyOn(idGeneratorAdapter, 'execute')
+
+        //act
+        await sut.execute(createTransactionParams)
+
+        //assert
+        expect(idGeneratorAdapterSpy).toHaveBeenCalled()
+    })
 })
