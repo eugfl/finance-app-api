9fcc8c62d2d131a2c049e3661eab889365b914e0
Gabriel Figueiredo <eugfl@hotmail.com>
2024-11-28 21:53:44 -0300

    test: ensure PostgresDeleteUserRepository calls Prisma with correct params

--- src/repositories/postgres/user/delete-user.test.js
+++ src/repositories/postgres/user/delete-user.test.js
@@ -17,4 +17,20 @@ describe('DeleteUserRepository', () => {
         //assert
         expect(result).toStrictEqual(user)
     })
+
+    it('should call Prisma with correct params', async () => {
+        //arrange
+        const sut = new PostgresDeleteUserRepository()
+        const prismaSpy = jest.spyOn(prisma.user, 'delete')
+
+        //act
+        await sut.execute(user.id)
+
+        //assert
+        expect(prismaSpy).toHaveBeenCalledWith({
+            where: {
+                id: user.id,
+            },
+        })
+    })
 })
