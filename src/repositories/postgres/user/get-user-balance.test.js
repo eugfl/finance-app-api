7253bc40d357b5f6e19980d0b2746f4c4a724cd7
Gabriel Figueiredo <eugfl@hotmail.com>
2024-11-29 14:47:36 -0300

    test: ensure PostgresGetUserBalanceRepository calls Prisma with correct params

--- src/repositories/postgres/user/get-user-balance.test.js
+++ src/repositories/postgres/user/get-user-balance.test.js
@@ -5,8 +5,8 @@ import { PostgresGetUserBalanceRepository } from './get-user-balance'
 
 describe('GetUserBalance', () => {
     it('should get user balance on db', async () => {
+        //arrange
         const user = await prisma.user.create({ data: fakeUser })
-
         await prisma.transaction.createMany({
             data: [
                 {
@@ -54,13 +54,53 @@ describe('GetUserBalance', () => {
             ],
         })
 
+        //act
         const sut = new PostgresGetUserBalanceRepository()
-
         const result = await sut.execute(user.id)
 
+        //assert
         expect(result.earnings.toString()).toBe('10000')
         expect(result.expenses.toString()).toBe('2000')
         expect(result.investments.toString()).toBe('6000')
         expect(result.balance.toString()).toBe('2000')
+    })
+
+    it('should call Prisma with correct params', async () => {
+        //arrange
+        const sut = new PostgresGetUserBalanceRepository()
+        const prismaSpy = jest.spyOn(prisma.transaction, 'aggregate')
+
+        //act
+        await sut.execute(fakeUser.id)
+
+        //assert
+        expect(prismaSpy).toHaveBeenCalledTimes(3)
+        expect(prismaSpy).toHaveBeenCalledWith({
+            where: {
+                user_id: fakeUser.id,
+                type: 'EXPENSE',
+            },
+            _sum: {
+                amount: true,
+            },
+        })
+        expect(prismaSpy).toHaveBeenCalledWith({
+            where: {
+                user_id: fakeUser.id,
+                type: 'EARNING',
+            },
+            _sum: {
+                amount: true,
+            },
+        })
+        expect(prismaSpy).toHaveBeenCalledWith({
+            where: {
+                user_id: fakeUser.id,
+                type: 'INVESTMENT',
+            },
+            _sum: {
+                amount: true,
+            },
+        })
     })
 })
