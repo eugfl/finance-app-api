0cac6f84ee885ffec40778a6e96740f915ac3555
Gabriel Figueiredo <eugfl@hotmail.com>
2024-12-02 23:15:06 -0300

    test: ensure PostgresDeleteTransactionRepository calls Prisma with correct params

--- src/repositories/postgres/transaction/delete-transaction.test.js
+++ src/repositories/postgres/transaction/delete-transaction.test.js
@@ -26,4 +26,20 @@ describe('PostgresDeleteTransactionRepository', () => {
         expect(dayjs(result.date).month()).toBe(dayjs(transaction.date).month())
         expect(dayjs(result.date).year()).toBe(dayjs(transaction.date).year())
     })
+
+    it('should call Prisma with correct params', async () => {
+        //arrange
+        const prismaSpy = jest.spyOn(prisma.transaction, 'delete')
+        const sut = new PostgresDeleteTransactionRepository()
+
+        //act
+        await sut.execute(transaction.id)
+
+        //assert
+        expect(prismaSpy).toHaveBeenCalledWith({
+            where: {
+                id: transaction.id,
+            },
+        })
+    })
 })
