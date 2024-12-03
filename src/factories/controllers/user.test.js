c64f33eaa6c5cb8e570eab5db0517e51cdac3f18
Gabriel Figueiredo <eugfl@hotmail.com>
2024-12-03 12:01:32 -0300

    test: ensure makeDeleteUserController returns valid DeleteUserController

--- src/factories/controllers/user.test.js
+++ src/factories/controllers/user.test.js
@@ -1,10 +1,12 @@
 import {
     CreateUserController,
+    DeleteUserController,
     GetUserByIdController,
     UpdateUserController,
 } from '../../controllers/index.js'
 import {
     makeCreateUserController,
+    makeDeleteUserController,
     makeGetUserByIdController,
     makeUpdateUserController,
 } from './user.js'
@@ -22,5 +24,9 @@ describe('UserControllerFactories', () => {
 
     it('should return a valid UpdateUserController instance', () => {
         expect(makeUpdateUserController()).toBeInstanceOf(UpdateUserController)
+    })
+
+    it('should return a valid DeleteUserController instance', () => {
+        expect(makeDeleteUserController()).toBeInstanceOf(DeleteUserController)
     })
 })
